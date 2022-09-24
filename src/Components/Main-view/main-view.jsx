7727249e5cf3b555ad583497ffb79348fb9b2895
massouty <massouty@outlook.com>
2022-09-24 08:46:42 +0200

    Update main-view.jsx
    If applied, this commit will have changed some codes.

--- src/Components/Main-view/main-view.jsx
+++ src/Components/Main-view/main-view.jsx
@@ -7,42 +7,41 @@ class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                {
-                    _id: 1,
-                    Title: "Silence of the Lambs",
-                    Description:
-                        "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
-                    ImagePath:"https://www.imdb.com/title/tt0102926/mediaviewer/rm803658241/?ref_=tt_md_3",
-                },
-                {
-                    _id: 2,
-                    Title: "Lord of the Ring",
-                    Description:
-                        " A  fellowship of hobbits , elves,dwarfs and men is formed to destroy the ring by casting it into the volcanic fire of the crack of doom .",
-                    ImagePath: "https://www.imdb.com/title/tt0120737/mediaviewer/rm3592958976/?ref_=tt_ov_i",
-                },
-                {
-                    _id: 3,
-                    Title: "Coco",
-                    Description:
-                        "The story follows a 12-years-old boy named Miguel who is accidentally transported to the Land of Dead.",
-                    ImagePath:"https://www.imdb.com/title/tt2380307/mediaviewer/rm585455872/?ref_=tt_ov_i",
-                },
-            ],
+            movies: [ ],
             selectedMovie: null,
         };
     }
-
+     componentDidMount() {
+    axios.get('https://melsflix.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
     setSelectedMovie = (movie) => {
         this.setState((prev) => ({
             ...prev,
             selectedMovie: movie,
         }));
+    }
+
+      onLoggedIn(user) {this.setState({user});}
+
+onRegistration(registered) {
+    this.setState({registered});
     };
 
+
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user, registered } = this.state;
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    if (!registered) return <RegistrationView onRegistration={(register) => this.onRegistration(register)} />;
 
         if (selectedMovie) {
             return (
