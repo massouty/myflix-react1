95cff22fdb6fe5678e055edaf32b14a2a65b6f22
massouty <massouty@outlook.com>
2022-09-24 13:30:21 +0200

    Update register.jsx
    If applied , this commit will have edited this file

---
+++ src/Components/Register/register.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {Form, Button, Card, CardGroup, Container, Col, Row, Navbar, Nav } from 'react-bootstrap';
+
+import './registration-view.scss';
+import axios from 'axios';
+
+export function RegistrationView(props) {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ birthday, setBirthday ] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password, email, birthday);
+        props.onRegistration(username);
+    };
+
+    return (
+    
+    <form className="container">
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+       E-mail:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+       birthday:
+        <input type="birthday" value={birthday} onChange={e => setBirthday (e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
