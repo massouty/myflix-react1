09f4520352e322a46a7775ea01a51522d9eed0a8
massouty <massouty@outlook.com>
2022-09-24 13:30:02 +0200

    Update login.jsx
    If applied , this commit will have edited this file

---
+++ src/Components/Login/login.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+ function Login (props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    
+  };
+
+  return (
+    <form className="container">
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
+
+export default Login;
