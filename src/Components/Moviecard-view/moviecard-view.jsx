d0d8f415c014298415d3b138a43a849471278be6
massouty <massouty@outlook.com>
2022-09-24 14:17:45 +0200

    Update moviecard-view.jsx
    If applied , this commit will have changed some codes

--- src/Components/Moviecard-view/moviecard-view.jsx
+++ src/Components/Moviecard-view/moviecard-view.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 class MovieCard extends React.Component {
-    constructor(){
-    super();
-    this.state = {
-      movies: [
-  {
-    _id: 1,
-    Title: 'Silence of the Lambs',
-    Description: 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.',
-    ImagePath: 'https://www.imdb.com/title/tt0102926/mediaviewer/rm803658241/?ref_=tt_md_3',
-
-  },
-  {
-    _id: 2,
-    Title: 'Lord of the Ring',
-    Description: ' A  fellowship of hobbits , elves,dwarfs and men is formed to destroy the ring by casting it into the volcanic fire of the crack of doom .',
-    ImagePath: 'https://www.imdb.com/title/tt0120737/mediaviewer/rm3592958976/?ref_=tt_ov_i',
-  
-  },
-  {
-    _id: 3,
-    Title: 'Coco',
-    Description: 'The story follows a 12-years-old boy named Miguel who is accidentally transported to the Land of Dead.',
-    ImagePath: "https://www.imdb.com/title/tt2380307/mediaviewer/rm585455872/?ref_=tt_ov_i",
-
-  },
-      ]
-    }
-}
   render() {
     const { movie, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+    return (
+      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+    );
   }
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+        })
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
 
 export default MovieCard;
