d7dc068dc6e19f612aac7107095fcbfb8755198d
massouty <massouty@outlook.com>
2022-09-25 15:40:05 +0200

    Update movie-view.jsx
    If applied , this commit will have added  bootstrap styles to this file .

--- src/Components/Movie-view/movie-view.jsx
+++ src/Components/Movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row } from 'react-bootstrap';
 
 import './movie-view.scss';
 
@@ -9,17 +10,24 @@ class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
+        <Row>
         <div className="movie-poster">
           <img src={movie.ImagePath} alt=""/>
         </div>
+        </Row>
+        <Row>
         <div className="movie-title">
           <span className="label">Title: </span>
           <span className="value">{movie.Title}</span>
         </div>
+        </Row>
+        <Row>
         <div className="movie-description">
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
+        </Row>
+        
         <button onClick={() => { onBackClick(null); }}>Back</button>
 
       </div>
