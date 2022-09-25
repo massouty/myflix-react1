d3d8f11f28e89759f3e1b4140e6bab68172c64a1
massouty <massouty@outlook.com>
2022-09-25 15:40:32 +0200

    Update index.jsx
    If applied , this commit will have added  bootstrap styles to this file .

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import MainView from './Components/Main-view/main-view';
+import Container from 'react-bootstrap/Container';
 
 class MyFlixApplication extends React.Component {
   render() {
     return (
+    <Container>
       <MainView />
+    </Container>
     );
   }
 }
