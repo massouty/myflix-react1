cf86564e85e3e11a7549601ddc02f6e181ed1151
massouty <massouty@outlook.com>
2022-09-25 15:40:20 +0200

    Update moviecard-view.jsx
    If applied , this commit will have added  bootstrap styles to this file .

--- src/Components/Moviecard-view/moviecard-view.jsx
+++ src/Components/Moviecard-view/moviecard-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import './moviecard-view.scss';
 
 class MovieCard extends React.Component {
@@ -7,8 +9,16 @@ class MovieCard extends React.Component {
     const { movie, onMovieClick } = this.props;
 
     return (
-      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+        <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
     );
+   
   }
 }
 
